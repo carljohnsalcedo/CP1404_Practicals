9512e2a37e0401f88c49296a493344ded3acd6c2
carljohnsalcedo <carl.salcedo@my.jcu.edu.au>
2017-05-16 21:34:27 +1000

    update prac09 stuff

--- Prac09/cleanupFiles.py
+++ Prac09/cleanupFiles.py
@@ -3,17 +3,25 @@ CP1404/CP5632 Practical
 File renaming and os examples
 """
 import os
-
 __author__ = 'Lindsay Ward'
 
 print("Current directory is", os.getcwd())
-
+changed_name = ""
 os.chdir('Lyrics/Christmas')
-
 for filename in os.listdir('.'):
     if not os.path.isdir(filename):
+        count = -1
         new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-        print(new_name)
+        new_example = new_name[0]
+        for letter in new_name[1:]:
+            if letter.isupper() and count != 0:
+                new_example += '_'
+            if letter.islower() and new_example[-1] == "_":
+                letter = letter.upper()
+            new_example += letter
+            count += 1
+        new_example = new_example.replace("__", "_").replace("(_", "(")
+        print(new_example)
         os.rename(filename, new_name)
 
 os.chdir('..')
